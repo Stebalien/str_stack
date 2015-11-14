33d34090475244623a03cc71fff9b2d2300347a1
Steven Allen <steven@stebalien.com>
2015-11-14 16:27:30 -0500

    Update docs for 89c6ae487a18dc23d4fdf84a5ca4ca4b55397db2
    Get documentation ready

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['str_stack'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='str_stack/struct.StrStack.html' title='str_stack::StrStack'>StrStack</a>",];
+implementors['str_stack'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='str_stack/struct.StrStack.html' title='str_stack::StrStack'>StrStack</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
